3d36c32a2ca1746c4d7e3f11c82bf3866ecb0a3b
UGLi <57222449+UGLimusic@users.noreply.github.com>
2021-04-21 19:53:54 +0200

    Fix basthon macro

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ from ipythonblocks import BlockGrid, colors
 
 
 def define_env(env):
-    "Hook function"
 
     @env.macro
     def basthon(exo: str, hauteur: int) -> str:
-
-        "Renvoie du HTML pour embarquer un fichier `exo` dans Basthon"
         path = env.variables.io_url + env.variables.page.url
         path = '/'.join([x for x in path.rstrip('/').split('/')[:-1]])+'/'+exo
-        return f"""**dir :** {os.getcwd()}
-        
-        **env.variables.io_url :** {env.variables.io_url}
-        
-        **env.variables.page.url : ** {env.variables.page.url}
-        
-        <iframe src="https://console.basthon.fr/?from={path}" width=100% height={hauteur}></iframe>"""
+        return f"""<iframe src="https://console.basthon.fr/?from={path}" width=100% height={hauteur}></iframe>"""
+
