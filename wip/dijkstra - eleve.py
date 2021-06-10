8be663871950807d8b0875c2e9d36a7a92c3f2c8
UGLi <57222449+UGLimusic@users.noreply.github.com>
2021-06-10 10:46:28 +0200

    dunno

---
+++ wip/dijkstra - eleve.py
@@ -0,0 +1,16 @@
+import networkx as nx
+
+
+
+def dijkstra(graph: nx.Graph, start)->dict:
+	pass
+
+
+def shortest_path(x,y)->list:
+        pass
+
+G = nx.Graph()
+G.add_weighted_edges_from(
+	[('A', 'B', 20), ('A', 'C', 6), ('B', 'D', 2), ('C', 'D', 17), ('D', 'E', 10), ('D', 'F', 15), ('E', 'F', 6),
+	 ('E', 'G', 2), ('F', 'G', 6)])
+
--- wip/dijkstra.py
+++ wip/dijkstra.py
@@ -8,81 +8,71 @@ VISITED = (.5, .5, 1)
 
 
 def dijkstra(graph: nx.Graph, start, end):
-    nodes = list(graph.nodes)
-    d = {node: float('inf') for node in nodes}
-    d[start] = 0
-    pred = dict()
-    while nodes:
-        print(f"Remaining nodes  and distances: {d}.")
-        a = min((n for n in d if n in nodes), key=lambda x: d[x])
-        nodes.remove(a)
-        print(f"closest : {a}")
-        for b in graph.neighbors(a):
-            if b in nodes and d[b] > d[a] + graph.edges[(a, b)]['weight']:
-                d[b] = d[a] + graph.edges[(a, b)]['weight']
-                pred[b] = a
+	nodes = list(graph.nodes)
+	d = {node: float('inf') for node in nodes}
+	d[start] = 0
+	pred = dict()
+	while nodes:
+		print(f"Remaining nodes  and distances: {d}.")
+		a = min((n for n in d if n in nodes), key=lambda x: d[x])
+		nodes.remove(a)
+		print(f"closest : {a}")
+		for b in graph.neighbors(a):
+			if b in nodes and d[b] > d[a] + graph.edges[(a, b)]['weight']:
+				d[b] = d[a] + graph.edges[(a, b)]['weight']
+				pred[b] = a
 
-    result = [end]
-    while pred[result[0]] != start:
-        result = [pred[result[0]]] + result
-    return [start] + result, d
+	result = [end]
+	while pred[result[0]] != start:
+		result = [pred[result[0]]] + result
+	return [start] + result, d
 
 
 def render_graph(g: nx.Graph, position=None, labels=None, colors=None, save=True) -> None:
-    # default values should not be mutable so :
+	# default values should not be mutable so :
 
-    position = position or nx.spring_layout(g)
-    labels = labels or {n: n for n in g.nodes}
-    colors = colors or {n: NOT_VISITED for n in g.nodes}
+	position = position or nx.spring_layout(g)
+	labels = labels or {n: n for n in g.nodes}
+	colors = colors or [NOT_VISITED for n in g.nodes]
 
-    plt.clf()
-    plt.axis("off")
+	plt.clf()
+	#plt.axis("off")
 
-    # draw nodes
+	# draw nodes
 
-    nx.draw_networkx_nodes(g, position, node_size=700, node_color=colors)
+	nx.draw_networkx_nodes(g, position, node_size=700, node_color=colors)
 
-    # draw edges
+	# draw edges
 
-    nx.draw_networkx_edges(G, pos)
+	nx.draw_networkx_edges(G, pos)
 
-    # draw node labels
+	# draw node labels
 
-    nx.draw_networkx_labels(g, position, labels, font_size=8, font_family="sans-serif", font_color="white")
+	nx.draw_networkx_labels(g, position, labels, font_size=8, font_family="sans-serif", font_color="white")
 
-    # draw edge weights
-    edge_labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_family="sans-serif", font_size=8)
-    if not save:
-        plt.show()
-    else:
-        plt.savefig('fig.svg')
+	# draw edge weights
+	edge_labels = nx.get_edge_attributes(G, 'weight')
+	nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_family="sans-serif", font_size=8)
+
+
+	if not save:
+		plt.show()
+	else:
+		plt.savefig('fig.svg')
 
 
 G = nx.Graph()
 
-G.add_weighted_edges_from([('A', 'B', 20), ('A', 'C', 6), ('B', 'D', 2), ('C', 'D', 17), ('D', 'E', 10), ('D', 'F', 15),
-                           ('E', 'F', 6), ('E', 'G', 2),
-                           ('F', 'G', 6)])
+G.add_weighted_edges_from(
+	[('A', 'B', 20), ('A', 'C', 6), ('B', 'D', 2), ('C', 'D', 17), ('D', 'E', 10), ('D', 'F', 15), ('E', 'F', 6),
+	 ('E', 'G', 2), ('F', 'G', 6)])
 
 # pos = nx.spring_layout(G)  # positions for all nodes
 
 
 pos = {'A': (0, 0), 'B': (2, 1), 'C': (2, -1), 'D': (4, 0), 'E': (6, 1), 'F': (6, -1), 'G': (8, 0)}
 labels = {n: n + ' : \u221e' for n in G.nodes}
-# nodes
-nx.draw_networkx_nodes(G, pos, node_size=700)
 
-# edges
-nx.draw_networkx_edges(G, pos)
+render_graph(G, position=pos, labels=labels)
 
-# labels
-nx.draw_networkx_labels(G, pos, labels, font_size=8, font_family="sans-serif", font_color="white")
-labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_family="sans-serif", font_size=8)
-plt.axis("off")
-plt.tight_layout()
-plt.savefig("hey.svg")
-L = nx.algorithms.shortest_paths.weighted.dijkstra_path(G, 'A', 'G')
-print(L)
 print(dijkstra(G, 'A', 'G'))
